57a0473481f38881782f6db80b49cf81dae77201
Vinay Aggarwal <aryans.vinay@gmail.com>
2015-05-13 23:05:42 +0530

    few more files has been added

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.less('app.less')
+    	.publish('/resources/less/app.less', 'public/app.css');
 });
